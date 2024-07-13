c5863c1153f46e86dace01d6d4c7a2fdcf6ef054
Sarkhan Huseynov <sarxanhuseynov521@gmail.com>
2024-07-13 19:55:08 +0400

    create a Shelter in the model

---
+++ back/src/main/java/az/edu/turing/hackaton/kucelerinsesi/model/Shelter.java
@@ -0,0 +1,78 @@
+package az.edu.turing.hackaton.kucelerinsesi.model;
+
+// Shelter entitisi
+public class Shelter {
+    private Long id;
+    private String companyName;
+    private String email;
+    private String password;
+    private String address;
+    private String phoneNumber;
+    private String description;
+    private String workHours;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getWorkHours() {
+        return workHours;
+    }
+
+    public void setWorkHours(String workHours) {
+        this.workHours = workHours;
+    }
+}
+
