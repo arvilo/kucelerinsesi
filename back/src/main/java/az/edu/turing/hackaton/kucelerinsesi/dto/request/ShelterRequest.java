ee712370f6dbaeb2ab446fa67ec2fb4108b0e52b
Sarkhan Huseynov <sarxanhuseynov521@gmail.com>
2024-07-14 03:59:11 +0400

    create ShelterRequest.java

---
+++ back/src/main/java/az/edu/turing/hackaton/kucelerinsesi/dto/request/ShelterRequest.java
@@ -0,0 +1,69 @@
+package az.edu.turing.hackaton.kucelerinsesi.dto.request;
+
+public class ShelterRequest {
+    private String companyName;
+    private String email;
+    private String password;
+    private String address;
+    private String phoneNumber;
+    private String description;
+    private String workHours;
+
+    // Getters and Setters
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getWorkHours() {
+        return workHours;
+    }
+
+    public void setWorkHours(String workHours) {
+        this.workHours = workHours;
+    }
+}
+
