30722893752f67c7eb526478000e7b876b7c4cf2
Sarkhan Huseynov <sarxanhuseynov521@gmail.com>
2024-07-14 03:58:48 +0400

    create ShelterFileException.java

---
+++ back/src/main/java/az/edu/turing/hackaton/kucelerinsesi/exception/ShelterNotFoundException.java
@@ -0,0 +1,7 @@
+package az.edu.turing.hackaton.kucelerinsesi.exception;
+
+public class ShelterNotFoundException extends RuntimeException {
+    public ShelterNotFoundException(String message) {
+        super(message);
+    }
+}
