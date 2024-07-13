84ba38fd46838cb677d688131570613447b61108
Sarkhan Huseynov <sarxanhuseynov521@gmail.com>
2024-07-14 03:59:27 +0400

    create ShelterResponse.java

---
+++ back/src/main/java/az/edu/turing/hackaton/kucelerinsesi/dto/response/ShelterResponse.java
@@ -0,0 +1,69 @@
+package az.edu.turing.hackaton.kucelerinsesi.dto.response;
+
+public class ShelterResponse {
+    private Long id;
+    private String companyName;
+    private String email;
+    private String address;
+    private String phoneNumber;
+    private String description;
+    private String workHours;
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getWorkHours() {
+        return workHours;
+    }
+
+    public void setWorkHours(String workHours) {
+        this.workHours = workHours;
+    }
+}
+
