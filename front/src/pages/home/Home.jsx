fe535a283f21754d032c6090089888bd3cf83661
MuxammedL <layicovmuxammed@gmail.com>
2024-07-14 19:48:20 +0400

    some changes

--- front/src/pages/home/Home.jsx
+++ front/src/pages/home/Home.jsx
@@ -2,13 +2,13 @@ import { getShelters } from "@/services/shelters";
 import React, { useEffect } from "react";
 
 const Home = () => {
-    useEffect(() => {
-        const fecthData = async () => {
-            const response = getShelters();
-            console.log(await response);
-        };
-        fecthData();
-    }, []);
+    // useEffect(() => {
+    //     const fecthData = async () => {
+    //         const response = getShelters();
+    //         console.log(await response);
+    //     };
+    //     fecthData();
+    // }, []);
     return <div>Home</div>;
 };
 
